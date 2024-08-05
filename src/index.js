17810ddbfe558e01209a0a0b573cd5e7c7278cd2
MonicaSus <52523112+MonicaSus@users.noreply.github.com>
2024-08-05 20:12:43 +0530

    Js file changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import DirectMain from './DirectMain';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <DirectMain />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
